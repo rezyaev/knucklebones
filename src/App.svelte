f776e4630aa5eaa5b24e4043e94a30e7d5b83ce2
Danil Rezyayev <danil.rezyaev@gmail.com>
2022-08-23 17:14:09 +0300

    create main layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,39 @@
 <script lang="ts">
-  import "./app.css";
+	import "./app.css";
 </script>
 
-<main class="w-screen h-screen bg-zinc-900">
+<main class="flex h-screen w-screen bg-zinc-900">
+	<div class="flex h-full flex-1 items-center justify-center">
+		<h2 class="text-2xl font-bold text-zinc-100">Player</h2>
+	</div>
+	<div
+		class="flex h-full flex-1 flex-col justify-between border-x-8 border-red-700 bg-stone-500 p-12"
+	>
+		<div class="grid h-2/5 grid-cols-3 grid-rows-3 gap-2">
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+		</div>
+
+		<div class="grid h-2/5 grid-cols-3 grid-rows-3 gap-2">
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+			<div class="bg-stone-800"></div>
+		</div>
+	</div>
+	<div class="flex h-full flex-1 items-center justify-center">
+		<h2 class="text-2xl font-bold text-zinc-100">AI</h2>
+	</div>
 </main>
