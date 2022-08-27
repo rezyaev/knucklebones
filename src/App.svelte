ed029931b8027ffcd79e36dc3734ed8df50fbab6
Danil Rezyayev <danil.rezyaev@gmail.com>
2022-08-27 13:58:43 +0300

    display player's score

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import "./app.css";
-	import { random } from "lodash-es";
+	import { random, sum } from "lodash-es";
 	import Dice from "./lib/Dice.svelte";
 	import Cell from "./lib/Cell.svelte";
 
@@ -14,15 +14,18 @@
 		currentDice = random(1, 6);
 	}
 
+	$: score = sum(cells);
+
 	$: {
 		console.log("[App State Updated]");
-		console.log({ currentDice, cells });
+		console.log({ currentDice, score, cells });
 	}
 </script>
 
 <main class="flex h-screen w-screen bg-zinc-900">
-	<div class="flex h-full flex-1 flex-col items-center justify-end gap-4 pb-36">
-		<h2 class="text-2xl font-bold text-zinc-100">Player</h2>
+	<div class="flex h-full flex-1 flex-col items-center justify-end pb-36">
+		<h2 class="mb-1 text-2xl font-bold text-zinc-100">Player</h2>
+		<h3 class="mb-6 text-2xl font-bold text-zinc-100">{score}</h3>
 		<div
 			class="flex h-24 w-3/4 items-center justify-center rounded-xl bg-stone-600"
 		>
@@ -47,7 +50,7 @@
 		</div>
 
 		<!-- Player Board -->
-		<div class="grid h-2/5 grid-cols-3 grid-rows-3 gap-2">
+		<div class="grid h-2/5 grid-flow-col grid-cols-3 grid-rows-3 gap-2">
 			{#each cells as cell, index}
 				<Cell dice="{cell}" on:click="{() => putDice(index)}" />
 			{/each}
